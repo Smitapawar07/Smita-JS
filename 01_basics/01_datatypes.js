56665b08ade786311207bfa413ba3590cc0f0a2b
Smita <133039946+Smitapawar07@users.noreply.github.com>
2024-04-17 08:50:44 +0000

    Datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as newer version ,for an good use
+//it is only run in the browser's console only it is not avilable right now so we can do this declaration then we get err
+//alert("hiii")
+//we are using nodejs , not browser 
+
+let name = "sdfgh"
+let age = 18
+let isLoggedIn = false
+
+//primitive data types
+
+//number => 2to power 53
+//bigint
+//string => ""
+//boolean => true / false
+//null => its a standalone value null is represent the empty value intentionally it is null
+//undefined => only declaration is there right now not assigning the value
+//symbol => it will comes in pic when we used the react in figma tools for unique ness
+
+//object
+
+
+console.log(typeof null);//its gives us the op object
+console.log(typeof undefined);//it gives us the op undefined
