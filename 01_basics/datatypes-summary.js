261ec87dbee6c4cd14e6c327d20f46892f6c2e97
Smita <133039946+Smitapawar07@users.noreply.github.com>
2024-04-18 07:06:18 +0000

    datatypes-

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,12 @@
+//in official documentation there is only two main datatypes
+//1.primitive 2.non-primitive/reference type
+//it is based on call by value call by reference
+
+//primitive: call by value 
+//7 types : String,Number,boolean,null,undefined,symbol(unique for component),bigint
+//JS is dynamically typed 
+/*JavaScript is a dynamically typed language, which means that data types of variables are determined by the value they hold at runtime and can change throughout the program as we assign different values to them. */
+
+
+// Non -primitive :reference will gives us
+// 3 types :array,objects , functions 
