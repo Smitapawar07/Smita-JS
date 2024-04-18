afec2befd7020a4227e12112f32a5c29c8afb71d
Smita <133039946+Smitapawar07@users.noreply.github.com>
2024-04-18 13:33:38 +0000

    array-01

---
+++ 02_basics/01_array.js
@@ -0,0 +1,18 @@
+/*initially i am facing the problem with my new folder so i did chatgbt and got the result
+@Smitapawar07 ➜ /workspaces/smita-JS (main) $ ls -l 02_basics/01_array.js
+-rw-rw-rw- 1 node node 30 Apr 18 13:06 02_basics/01_array.js
+@Smitapawar07 ➜ /workspaces/smita-JS (main) $ chmod +x 02_basics/01_array.js
+@Smitapawar07 ➜ /workspaces/smita-JS (main) $ node 02_basics/01_array.js
+1
+@Smitapawar07 ➜ /workspaces/smita-JS (main) $ 
+ */
+
+
+// const num =1
+// console.log(num);
+
+
+
+//array
+const myArr = [0,1,2,3,4]
+console.log(myArr[0]);
