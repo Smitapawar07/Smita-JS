e30c1d1e36978e06cc7f165f83bddf563c15f27b
Smita <133039946+Smitapawar07@users.noreply.github.com>
2024-04-18 07:18:30 +0000

    datatype summary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -8,5 +8,32 @@
 /*JavaScript is a dynamically typed language, which means that data types of variables are determined by the value they hold at runtime and can change throughout the program as we assign different values to them. */
 
 
+
+
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+const id= Symbol('123')
+const anotherId = Symbol('123')
+//so we will get the op false because we know that symbol creates the unique id
+console.log(id === anotherId);
+
+
+
 // Non -primitive :reference will gives us
 // 3 types :array,objects , functions 
+const heros =["srk","sdk","dcc"]//curly brackets me objects
+let myObj = {
+    name:"smita",
+    age : 33,
+}
+
+//function we treat it as a var
+const myFunction =function{
+    console.log("hii")
+}
+
+//datatype of fun is fiunction object
+// non pri data type is object
