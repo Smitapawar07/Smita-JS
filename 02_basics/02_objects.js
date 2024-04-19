0c2937c2b61c0dbb08dbf98eacda4398a3c19a29
Smita <133039946+Smitapawar07@users.noreply.github.com>
2024-04-19 08:51:49 +0000

    Part 02 Objects

---
+++ 02_basics/02_objects.js
@@ -0,0 +1,8 @@
+//how can we declare the singleton value by using the constructor
+
+//const tinderUser ={} //op same {}//this is non singleton obj
+const tinderUser =new Object() //this is sinlgeton obj
+tinderUser.id="3223"
+tinderUser.name ="sddc"
+tinderUser.
+console.log(tinderUser);//op is {}
