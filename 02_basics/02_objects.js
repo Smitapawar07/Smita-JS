5254fb7cf860ff1377faafca613d550a46b9ad99
Smita <133039946+Smitapawar07@users.noreply.github.com>
2024-04-20 09:58:00 +0000

    obj part 2

--- 02_basics/02_objects.js
+++ 02_basics/02_objects.js
@@ -1,8 +1,29 @@
 //how can we declare the singleton value by using the constructor
 
-//const tinderUser ={} //op same {}//this is non singleton obj
-const tinderUser =new Object() //this is sinlgeton obj
-tinderUser.id="3223"
-tinderUser.name ="sddc"
-tinderUser.
-console.log(tinderUser);//op is {}
+// const tinderUser ={} //op same {}//this is non singleton obj
+// //const tinderUser =new Object() //this is sinlgeton obj
+// tinderUser.id="3223"
+// tinderUser.name ="sddc"
+// tinderUser.isLoggedIn = false
+// console.log(tinderUser);//op is {}
+
+//we can give objects as a nested one
+
+const regUser = {
+    email : "dfcgh@.com",
+    fullname:{
+        userfullname:{
+            name:"sd"
+        }
+    }
+}
+//console.log(regUser.fullname.userfullname);
+
+const obj1 ={1:"a",2:"d"}
+const obj2 ={3:"k",4:"n"}
+const obj3 ={5:"g",6:"l"}
+//const obj4 ={obj1,obj2,obj3}
+const obj4 =Object.assign({},obj1,obj2,obj3)
+console.log(obj4);
+
+//here we want to combine the obj ele but we facing the pb as same as arr so by assigning the value we can easily combine the value
