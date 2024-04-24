23d0284e34578649d14048f1d125a3ca15a567ad
Smita <133039946+Smitapawar07@users.noreply.github.com>
2024-04-24 07:09:45 +0000

    all basic control

--- 04_controlflow/03_truthy.js
+++ 04_controlflow/03_truthy.js
@@ -14,7 +14,26 @@
 //
 
 
-const emptyObj={}
-    if (Object.keys(emptyObj).length === 0) {
-        console.log("obj is empty");
-    }
+// const emptyObj={}
+//     if (Object.keys(emptyObj).length === 0) {
+//         console.log("obj is empty");
+//     }
+
+
+    //Nullish Coalescing Operator (??): null undefined
+    let val1;
+    //val1 = 21 ?? 10
+    //val1= null ?? 2
+    //val1 = undefined ?? 1234
+
+    val1 = null ?? 10 ?? 20
+
+    console.log(val1);
+
+
+    //terniary operator
+   // condition ? true:false
+   
+
+   const iceTeaPricec = 1009
+   iceTeaPricec<=80 ? console.log("less than 80"):console.log("more than 80");
