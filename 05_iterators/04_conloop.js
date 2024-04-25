06f697a8e4fd1ba716f7398b565eda77e5a96bc7
Smita <133039946+Smitapawar07@users.noreply.github.com>
2024-04-25 07:36:20 +0000

    forof n forin loops

---
+++ 05_iterators/04_conloop.js
@@ -0,0 +1,34 @@
+const myObj={
+    js:"javascript",
+    cpp:"c++",
+    rb:"ruby",
+    sswift:"swift by apple"
+}
+
+//so here for obj there is fooin loop also it is used for others also
+
+// for (const key in myObj) {
+//     console.log(`${key} shortcut is for ${myObj[key]}`);
+// }
+//for getting the keys we can just write the obj and key 
+
+
+
+
+//for arr
+
+// const programming=["js","rb","cpp"]
+// for (const key in programming) {
+//     console.log(key);
+    
+// }
+// for (const key in programming) {
+//     console.log(programming[key]);
+// }
+//there is the keys for array also it starts from the 0 1 2...
+
+
+
+//map is not iteratable by the forin 
+
+
