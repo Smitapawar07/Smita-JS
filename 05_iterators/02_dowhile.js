a98101b42fc5581269687ef3ad069ab41078c0cf
Smita <133039946+Smitapawar07@users.noreply.github.com>
2024-04-25 06:56:36 +0000

    do while

---
+++ 05_iterators/02_dowhile.js
@@ -0,0 +1,25 @@
+let i = 0
+// while (i<=9) {
+//     console.log(`value of i ${i}`);
+//     i=i+2
+    
+// }
+
+
+
+// let myArr=['sas','fvv','fdv']
+// let arr=0
+// while (arr<myArr.length) {
+//     console.log(`value is ${myArr[arr]}`);
+//     arr=arr+1//arr++
+    
+// }
+
+
+//do-while
+
+let score=1
+do{
+console.log(`score is ${score}`);
+score++
+}while (score <=10);
