449609442afda313a46885f82e90197b9b3535bc
Smita <133039946+Smitapawar07@users.noreply.github.com>
2024-06-06 04:48:45 +0000

    promise

---
+++ 09_advance/01_promises.js
@@ -0,0 +1,7 @@
+// <!-- we are learning about the  -->
+
+//  <!-- Q and BlueBird we can use this libraries 
+//   bulebird vs async and wait
+//   but now we dont need this libraries-->
+
+  const promisesOne = new Promise()//
