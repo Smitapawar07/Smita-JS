58290db4ec5c923ba4a501ea94e4a2917f21724b
Smita <133039946+Smitapawar07@users.noreply.github.com>
2024-06-07 08:53:15 +0000

    promises done

--- 09_advance/01_promises.js
+++ 09_advance/01_promises.js
@@ -37,7 +37,7 @@ promiseThree.then(function(user){
 })
 const promiseFour = new Promise(function(resolve,reject){
     setTimeout(function(){
-        let error = true
+        let error = false
         if(!error){
             resolve({username:'smita', pass: '1234'})
         }else{
@@ -45,8 +45,62 @@ const promiseFour = new Promise(function(resolve,reject){
         }
     },1000)
 })
-const username = promiseFour.then((user) => {
+promiseFour
+.then((user) => {
     console.log(user);
     return user.username
 
+})
+.then((username) => {
+console.log(username);
+})
+.catch(function(error){
+    console.log(error);
 })  
+.finally(() => {
+    console.log("promise resolved");
+})
+
+ const promiseFive = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let error = true
+        if(!error){
+            resolve({username:'tushar', pass: '123'})
+        }else{
+            reject('error something went wrong')
+        }
+    },1000)
+ });
+//as per then there will be async and await it is used in db most it will some issue in the catch handling 
+
+async function consumePomiseFive(){
+   try {
+    const response = await promiseFive
+    console.log(response);
+   } catch (error) {
+    console.log(error);
+   }
+
+}
+consumePomiseFive()
+
+// async function getAllusers(){
+// try {
+//     const response= await fetch('https://jsonplaceholder.typecode.com/users')
+// const data=await response.json()
+// console.log(data);
+// } catch (error) {
+//     console.log("e:" ,error);
+// }
+// }
+//getAllusers()
+
+fetch('https://jsonplaceholder.typecode.com/users')
+.then((response) =>{
+return response.json()
+})
+.then((data) =>{
+    console.log(data);
+
+})
+.catch((error) => {console.log(error);})
